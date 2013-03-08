9d055324284fba8a8671f24c2eb13b839ea54ddd
Dafke <Dafke@dd6a41e9-cf23-bd40-98f3-70dd80662914>
2013-03-08 10:52:05 +0000

    refactor Transactions: - reorder functions
    git-svn-id: file:///home/david/repository/Accounting@173 dd6a41e9-cf23-bd40-98f3-70dd80662914

--- src/main/java/be/dafke/Accounting/Objects/Accounting/Transaction.java
+++ src/main/java/be/dafke/Accounting/Objects/Accounting/Transaction.java
@@ -11,66 +11,116 @@ import java.util.Calendar;
  * @see Booking
  */
 public class Transaction {
-	private BigDecimal debettotaal, credittotaal;
-	private String description = "";
-	private Calendar datum = null;
+	private BigDecimal debitTotal;
+    private BigDecimal creditTotal;
+    private Journal journal;
+
+    private boolean booked;
+    private int nrOfDebits = 0;
+
+    private String description = "";
+    private Calendar date = null;
     private int id;
     private String abbreviation;
+
     private final ArrayList<Booking> bookings;
-    private boolean booked;
-	private final ArrayList<Mortgage> mortgages;
-    private Journal journal;
-    private int nrOfDebits = 0;
+    private final ArrayList<Mortgage> mortgages;
 
     public Transaction() {
 		booked = false;
-		debettotaal = new BigDecimal(0);
-		debettotaal = debettotaal.setScale(2);
-		credittotaal = new BigDecimal(0);
-		credittotaal = credittotaal.setScale(2);
+		debitTotal = new BigDecimal(0);
+		debitTotal = debitTotal.setScale(2);
+		creditTotal = new BigDecimal(0);
+		creditTotal = creditTotal.setScale(2);
         bookings = new ArrayList<Booking>();
 		mortgages = new ArrayList<Mortgage>();
 	}
 
+    protected void lowerID() {
+        id--;
+    }
+
+    protected void raiseID() {
+        id++;
+    }
+
+    public void moveTransaction(Journal oldJournal, Journal newJournal) {
+        if (booked) {
+            oldJournal.unbook(this);
+        }
+        newJournal.book(this);
+    }
+
+    public void deleteTransaction(Journal journal) {
+        if (booked) journal.unbook(this);
+    }
+
+    public void book(Journal journal) {
+        // TODO: null check on journal OR Disable "Debit" and "Credit" button if there are no Journals
+        // if(journal == null) throw Exception --> catch Exception in GUI
+        journal.book(this);
+        this.journal = journal;
+        booked = true;
+        for(Mortgage mortgage : mortgages) {
+            mortgage.increasePayed();
+        }
+    }
+
+    // Getters (without setters)
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public Journal getJournal() {
+        return journal;
+    }
+
 	public BigDecimal getDebetTotaal() {
-		return debettotaal;
+		return debitTotal;
 	}
 
 	public BigDecimal getCreditTotaal() {
-		return credittotaal;
+		return creditTotal;
 	}
+
+    // Getters
 
     public String getAbbreviation() {
         return abbreviation;
     }
 
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
+    public int getId(){
+        return id;
     }
 
     public String getDescription(){
         return description;
     }
 
+    public Calendar getDate() {
+        return date;
+    }
+
+    // Setters
+
+    public void setId(int nr) {
+        id = nr;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	protected void lowerID() {
-        id--;
-	}
-
-	protected void raiseID() {
-        id++;
-	}
-
-	public Calendar getDate() {
-		return datum;
-	}
-
 	public void setDate(Calendar date) {
-		datum = date;
+		this.date = date;
 	}
+
+    // Adders
 
 	public void addMortgage(Mortgage mortgage) {
 		mortgages.add(mortgage);
@@ -84,50 +134,12 @@ public class Transaction {
         if(debit){
             bookings.add(nrOfDebits, booking);
             nrOfDebits++;
-            debettotaal = debettotaal.add(amount);
-            debettotaal = debettotaal.setScale(2);
+            debitTotal = debitTotal.add(amount);
+            debitTotal = debitTotal.setScale(2);
         } else {
             bookings.add(booking);
-            credittotaal = credittotaal.add(amount);
-            credittotaal = credittotaal.setScale(2);
+            creditTotal = creditTotal.add(amount);
+            creditTotal = creditTotal.setScale(2);
         }
-    }
-
-    public void moveTransaction(Journal oldJournal, Journal newJournal) {
-		if (booked) {
-			oldJournal.unbook(this);
-		}
-		newJournal.book(this);
-	}
-
-	public void deleteTransaction(Journal journal) {
-		if (booked) journal.unbook(this);
-	}
-
-	public void book(Journal journal) {
-        // TODO: null check on journal OR Disable "Debit" and "Credit" button if there are no Journals
-        // if(journal == null) throw Exception --> catch Exception in GUI
-		journal.book(this);
-        this.journal = journal;
-		booked = true;
-		for(Mortgage mortgage : mortgages) {
-			mortgage.increasePayed();
-		}
-	}
-
-    public int getId(){
-        return id;
-    }
-
-	public void setId(int nr) {
-        id = nr;
-	}
-
-	public ArrayList<Booking> getBookings() {
-        return bookings;
-    }
-
-    public Journal getJournal() {
-        return journal;
     }
 }
