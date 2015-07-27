1fb42fb348f9a60bba6084b0f0cf57adfd0a60e6
unknown <ddanneels@DIENB183.clear2pay.com>
2015-07-27 20:30:57 +0200

    update seed

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -2,7 +2,7 @@ mavenJob("01_UnitTests"){
     triggers {
         scm('H/15 * * * *')
     }
-    git("https://github.com/DavidCADanneels/Accounting.git")
+    git("https://github.com/DavidCADanneels/Accounting.git", null)
 //    rootPOM("pom.xml")
     goals("clean package")
 }
