1891057f81de2f8f4c49502c1a0ab4b3d4d0e505
unknown <ddanneels@DIENB183.clear2pay.com>
2015-07-27 20:29:05 +0200

    update seed

---
+++ Utils/src/test/java/be/dafke/Utils/MultiValueMapTest.java
@@ -0,0 +1,91 @@
+package be.dafke.Utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by ddanneels on 25/07/2015.
+ */
+public class MultiValueMapTest {
+    public static final String KEY1 = "Key1";
+    public static final String KEY2 = "Key2";
+    public static final String KEY3 = "Key3";
+    public static final String KEY4 = "Key4";
+    public static final String VALUE1 = "Value1";
+    public static final String VALUE2 = "Value2";
+    public static final String VALUE3 = "Value3";
+    public static final String VALUE4 = "Value4";
+    public static final String VALUE5 = "Value5";
+    public static final String VALUE6 = "Value6";
+    public static final String VALUE7 = "Value7";
+    public static final String VALUE8 = "Value8";
+    public static final String VALUE9 = "Value9";
+    private MultiValueMap<String, String> map;
+
+    @Before
+    public void init(){
+        map = new MultiValueMap<String, String>();
+    }
+
+    private void createDefaultMap(){
+        // K1: V(1-2)
+        // K2: V(3-4)
+        // K3: V(5-6-7)
+        // K4: V(8-9)
+        map.addValue(KEY1, VALUE1);
+        map.addValue(KEY1, VALUE2);
+
+        map.addValue(KEY2, VALUE3);
+        map.addValue(KEY2, VALUE4);
+
+        map.addValue(KEY3, VALUE5);
+        map.addValue(KEY3, VALUE6);
+        map.addValue(KEY3, VALUE7);
+
+        map.addValue(KEY4, VALUE8);
+        map.addValue(KEY4, VALUE9);
+    }
+
+    @Test
+    public void addValuesWithSameKey(){
+
+        assertEquals(2, map.get(KEY1).size());
+        assertTrue(map.get(KEY1).contains(VALUE1));
+        assertTrue(map.get(KEY1).contains(VALUE2));
+    }
+
+    @Test
+    public void totalSizeOfMap(){
+        assertEquals(2, map.values().size());
+    }
+
+    @Test
+    public void addValuesWithDifferentKey(){
+        map.addValue(KEY1, VALUE1);
+        map.addValue(KEY2, VALUE2);
+        assertEquals(2, map.values().size());
+        assertEquals(1, map.get(KEY1).size());
+        assertEquals(1, map.get(KEY2).size());
+        assertTrue(map.get(KEY1).contains(VALUE1));
+        assertTrue(map.get(KEY2).contains(VALUE2));
+    }
+
+    @Test
+    public void tailList(){
+        map.addValue(KEY1, VALUE1);
+        map.addValue(KEY2, VALUE2);
+        map.addValue(KEY2, VALUE3);
+        assertEquals(3, map.tailList(KEY1, true).size());
+        assertEquals(2, map.tailList(KEY1, false).size());
+        assertEquals(2, map.tailList(KEY2,true).size());
+        assertEquals(0, map.tailList(KEY2,false).size());
+        map.removeValue(KEY2, VALUE2);
+
+    }
+
+}
--- seed/seed.groovy
+++ seed/seed.groovy
@@ -2,6 +2,7 @@ mavenJob("01_UnitTests"){
     triggers {
         scm('H/15 * * * *')
     }
+    git("https://github.com/DavidCADanneels/Accounting.git")
 //    rootPOM("pom.xml")
     goals("clean package")
 }
