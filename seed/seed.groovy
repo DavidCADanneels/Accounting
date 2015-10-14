3fcdd4c108bc20fdf417d200cf406fa8169591f2
David C.A. Danneels <daviddafke@msn.com>
2015-10-14 20:33:56 +0200

    archive artifacts

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -13,4 +13,10 @@ mavenJob("01_UnitTests"){
     }
 //    rootPOM("pom.xml")
     goals "clean package"
+    publishers {
+        archiveArtifacts {
+            pattern('**/*.jar')
+            onlyIfSuccessful()
+        }
+    }
 }
