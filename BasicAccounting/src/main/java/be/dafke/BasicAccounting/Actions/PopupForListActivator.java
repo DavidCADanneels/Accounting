2aa2c3c8949778f792bd16b71a28fc33160ee21e
unknown <ddanneels@DIENB183.clear2pay.com>
2015-12-10 16:13:48 +0100

    add defaultAction in ListPopup

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Actions/PopupForListActivator.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Actions/PopupForListActivator.java
@@ -1,14 +1,13 @@
 package be.dafke.BasicAccounting.Actions;
 
-import be.dafke.ComponentModel.RefreshableTable;
 import be.dafke.ObjectModel.BusinessObject;
 
 import javax.swing.JList;
 import javax.swing.JPopupMenu;
 import java.awt.Point;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
 /**
  * Created by ddanneel on 18/02/2015.
@@ -16,18 +15,30 @@ import java.util.ArrayList;
 public class PopupForListActivator extends MouseAdapter {
     private final JList<? extends BusinessObject> list;
     private final JPopupMenu popup;
+    private ActionListener defaultAction;
 
     public PopupForListActivator(JPopupMenu popup, JList<? extends BusinessObject> list) {
-        this.popup = popup;
-        this.list = list;
+        this(popup, list, null);
     }
 
+    public PopupForListActivator(JPopupMenu popup, JList<? extends BusinessObject> list, ActionListener defaultAction) {
+        this.popup = popup;
+        this.list = list;
+        this.defaultAction = defaultAction;
+    }
+
+
     public void mouseClicked(MouseEvent me) {
-        Point location = me.getLocationOnScreen();
-        if (me.getButton() == 3 && list.getSelectedIndex() != -1) {
-            popup.show(null, location.x, location.y);
-        } else {
-            popup.setVisible(false);
+        // TODO: if double-clicked: execute default action
+        if(defaultAction!=null && me.getClickCount()==2){
+            defaultAction.actionPerformed(null);
+        }else {
+            Point location = me.getLocationOnScreen();
+            if (me.getButton() == 3 && list.getSelectedIndex() != -1) {
+                popup.show(null, location.x, location.y);
+            } else {
+                popup.setVisible(false);
+            }
         }
     }
 }
