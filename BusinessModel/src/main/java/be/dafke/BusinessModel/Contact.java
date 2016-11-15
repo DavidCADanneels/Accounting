6575ff05e494ccac4d9fc0471da222467a503b8a
DavidCADanneels <David.Danneels@fisglobal.com>
2016-11-15 23:14:50 +0100

    create object, collection and translations

---
+++ BusinessModel/src/main/java/be/dafke/BusinessModel/Contact.java
@@ -0,0 +1,34 @@
+package be.dafke.BusinessModel;
+
+import be.dafke.ObjectModel.BusinessObject;
+
+/**
+ * Created by ddanneels on 15/11/2016.
+ */
+public class Contact extends BusinessObject{
+    private String name, addressLine1, addressLine2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+}
