b1c58b4a1f224bb7ae2b9cd59d7e835a935cf31e
DavidCADanneels <David.Danneels@fisglobal.com>
2016-11-25 10:38:09 +0100

    cleanup

--- BusinessModel/src/main/java/be/dafke/BusinessModel/Project.java
+++ BusinessModel/src/main/java/be/dafke/BusinessModel/Project.java
@@ -75,64 +75,9 @@ public class Project extends BusinessCollection<Account> implements MustBeRead {
 
 	public Balance getResultBalance() {
 		return resultBalance;
-//		return accounting.getBalances().createResultBalance(projectAccounts);
 	}
 
 	public Balance getRelationsBalance() {
 		return relationsBalance;
-//		return accounting.getBalances().createRelationsBalance(projectAccounts);
 	}
-
-//	public ArrayList<Transaction> getTransactions() {
-//		ArrayList<Transaction>  transactions = new ArrayList<> ();
-//		for(Account account:getBusinessObjects()){
-//			for(Movement movement :account.getBusinessObjects()){
-//				Transaction transaction = movement.getBooking().getTransaction();
-//				transactions.add(transaction);
-//			}
-//		}
-//		return transactions;
-//	}
-
-
-//	public Account close() {
-//		Transaction transaction = new Transaction();
-//        transaction.setDate(Calendar.getInstance());
-//        transaction.setDescription(getBundle("Projects").getString("CLOSE_PROJECT"));
-//		ArrayList<Account> teVerwijderen = new ArrayList<Account>();
-//
-//		BigDecimal totaalKost = new BigDecimal(0);
-//		for (Account kost : rekeningen.getAccounts(AccountType.Cost)) {
-//			BigDecimal amount = kost.getSaldo();
-//			transaction.addBooking(kost, amount,false,false);
-//			totaalKost = totaalKost.add(amount);
-//			teVerwijderen.add(kost);
-//		}
-//
-//		BigDecimal totaalOpbrengst = new BigDecimal(0);
-//		for (Account opbrengst : rekeningen.getAccounts(AccountType.Revenue)) {
-//			BigDecimal amount = opbrengst.getSaldo();
-//			transaction.addBooking(opbrengst,amount,true,false);
-//			totaalOpbrengst = totaalOpbrengst.add(amount);
-//			teVerwijderen.add(opbrengst);
-//		}
-//		Account result = null;
-//        // TODO ask user to provide a Name instead of autocreating --> catch Exception from creation
-////		if (totaalOpbrengst.compareTo(totaalKost) > 0) {
-////			BigDecimal winst = totaalOpbrengst.subtract(totaalKost);
-////			winst = winst.setScale(2);
-////			result = accounting.getBusinessObjects().add(getBundle("Projects").getString("GAIN_PROJECT") + naam, AccountType.Revenue);
-////			transaction.crediteer(result, winst);
-////		} else {
-////			BigDecimal verlies = totaalKost.subtract(totaalOpbrengst);
-////			verlies = verlies.setScale(2);
-////			result = accounting.getBusinessObjects().add(getBundle("Projects").getString("LOSS_PROJECT") + naam, AccountType.Cost);
-////			transaction.debiteer(result, verlies);
-////		}
-//		// TODO: transaction.addBusinessObject() !!!
-//		for(Account account : teVerwijderen) {
-//			rekeningen.remove(account);
-//		}
-//		return result;
-//	}
 }
