f70225cc10f8f825a11031f7f821c949daf765d5
DavidCADanneels <David.Danneels@fisglobal.com>
2017-05-21 04:01:28 +0200

    ACC-22 split name and abbr. in 2 columns

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/JournalManagementTableModel.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/JournalManagementTableModel.java
@@ -19,8 +19,9 @@ public class JournalManagementTableModel extends SelectableTableModel<Journal> {
      */
     private static final long serialVersionUID = 1L;
     public static final int NAME_COL = 0;
-    public static final int TYPE_COL = 1;
-    public static final int NEXT_ID_COL = 2;
+    public static final int ABBR_COL = 1;
+    public static final int TYPE_COL = 2;
+    public static final int NEXT_ID_COL = 3;
     private HashMap<Integer, String> columnNames = new HashMap<>();
     private HashMap<Integer, Class> columnClasses = new HashMap<>();
     private final Journals journals;
@@ -32,13 +33,15 @@ public class JournalManagementTableModel extends SelectableTableModel<Journal> {
     }
 
     private void createColumnClasses() {
-        columnClasses.put(NAME_COL, Journal.class);
+        columnClasses.put(NAME_COL, String.class);
+        columnClasses.put(ABBR_COL, String.class);
         columnClasses.put(TYPE_COL, String.class);
         columnClasses.put(NEXT_ID_COL, Integer.class);
     }
 
     private void createColumnNames() {
-        columnNames.put(NAME_COL, getBundle("Accounting").getString("JOURNAL_NAME"));
+        columnNames.put(NAME_COL, getBundle("Accounting").getString("NAME"));
+        columnNames.put(ABBR_COL, getBundle("Accounting").getString("ABBR"));
         columnNames.put(TYPE_COL, getBundle("Accounting").getString("TYPE"));
         columnNames.put(NEXT_ID_COL, getBundle("Accounting").getString("NEXT_INDEX"));
     }
@@ -54,7 +57,9 @@ public class JournalManagementTableModel extends SelectableTableModel<Journal> {
     public Object getValueAt(int row, int col) {
         Journal journal = journals.getBusinessObjects().get(row);
         if (col == NAME_COL) {
-            return journal;
+            return journal.getName();
+        } else if (col == ABBR_COL) {
+            return journal.getAbbreviation();
         } else if (col == TYPE_COL) {
             return journal.getType();
         } else if (col == NEXT_ID_COL) {
