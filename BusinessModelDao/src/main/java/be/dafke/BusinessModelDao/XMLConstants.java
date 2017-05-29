843fa1883d1efc6dd260f4f56e1c1c49bfde2dc8
DavidCADanneels <David.Danneels@fisglobal.com>
2017-05-29 21:25:02 +0200

    ACC-12 remove unused constants

--- BusinessModelDao/src/main/java/be/dafke/BusinessModelDao/XMLConstants.java
+++ BusinessModelDao/src/main/java/be/dafke/BusinessModelDao/XMLConstants.java
@@ -18,8 +18,6 @@ public class XMLConstants {
     public static final String LEFT_LIST = "leftList";
     public static final String RIGHT_LIST = "rightList";
     public static final String TYPES = "types";
-    public static final String DEBIT_TYPES = "debitTypes";
-    public static final String CREDIT_TYPES = "creditTypes";
     public static final String SINGLE_ACCOUNT = "singleAccount";
     public static final String VATTYPE = "vatType";
     public static final String ABBREVIATION = "abbr";// TODO: 'abbr' or 'abbreviation'
