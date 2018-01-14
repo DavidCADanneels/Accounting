c2a5b188e7a8bda995bcea1cc4a69f3e382c0953
DavidCADanneels <daviddafke@msn.com>
2018-01-14 12:16:56 +0100

    remove buttons from JournalsGUI

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/JournalsGUI.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/JournalsGUI.java
@@ -10,94 +10,44 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static be.dafke.BasicAccounting.Journals.JournalManagementGUI.showJournalManager;
 import static java.util.ResourceBundle.getBundle;
 
 /**
  * @author David Danneels
  */
-public class JournalsGUI extends JPanel implements ActionListener {
+public class JournalsGUI extends JPanel implements ActionListener{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JComboBox<Journal> combo;
-	private final JButton journalManagement, details;
-	private Journals journals;
-	private JournalTypes journalTypes;
-	private Accounts accounts;
-	private Accounting accounting;
-	private AccountTypes accountTypes;
 	private JournalInputGUI journalInputGUI;
 	private JournalGUI journalGUI;
 
 	public JournalsGUI(JournalGUI journalGUI, JournalInputGUI journalInputGUI){//Accounts accounts,  ) {
 		this.journalInputGUI=journalInputGUI;
 		this.journalGUI = journalGUI;
-//		this.accounts = accounts;
 		setBorder(new TitledBorder(new LineBorder(Color.BLACK), getBundle(
                 "Accounting").getString("JOURNALS")));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		combo = new JComboBox<>();
-        combo.addActionListener(e -> {
-			Journal newJournal = (Journal) combo.getSelectedItem();
-			Journal journal = journalInputGUI.switchJournal(newJournal);
-			Main.setJournal(journal);
-		});
+        combo.addActionListener(this);
 		combo.setEnabled(false);
 		add(combo);
-
-		JPanel paneel = new JPanel();
-
-		journalManagement = new JButton(getBundle("Accounting").getString("MANAGE_JOURNALS"));
-		journalManagement.addActionListener(e -> {
-			if(journals!=null) {
-				showJournalManager(accounts, journals, journalTypes, accountTypes).setVisible(true);
-			}
-		});
-		journalManagement.setEnabled(false);
-		details = new JButton(getBundle("Accounting").getString("VIEW_JOURNAL_DETAILS"));
-		details.addActionListener(e -> {
-			if(journals!=null) {
-				JournalDetails.getJournalDetails(accounting.getActiveJournal(), journals, journalInputGUI);
-			}
-		});
-
-		details.setEnabled(false);
-
-		paneel.add(journalManagement);
-		paneel.add(details);
-		add(paneel);
 	}
 
 	public void actionPerformed(ActionEvent ae) {
-
+		Journal newJournal = (Journal) combo.getSelectedItem();
+		Journal journal = journalInputGUI.switchJournal(newJournal);
+		Main.setJournal(journal);
 	}
 
 	public void setAccounting(Accounting accounting) {
-		this.accounting = accounting;
-		setAccounts(accounting==null?null:accounting.getAccounts());
-		setJournalTypes(accounting==null?null:accounting.getJournalTypes());
-		setAccountTypes(accounting==null?null:accounting.getAccountTypes());
 		setJournals(accounting==null?null:accounting.getJournals());
 		setJournal(accounting==null?null:accounting.getActiveJournal());
 	}
 
-	public void setAccounts(Accounts accounts) {
-		this.accounts = accounts;
-	}
-
-	public void setJournalTypes(JournalTypes journalTypes) {
-		this.journalTypes = journalTypes;
-	}
-
-	public void setAccountTypes(AccountTypes accountTypes) {
-		this.accountTypes = accountTypes;
-	}
-
 	public void setJournals(Journals journals){
-		this.journals = journals;
-
 		combo.removeActionListener(this);
 		combo.removeAllItems();
 		if (journals!=null) {
@@ -107,7 +57,6 @@ public class JournalsGUI extends JPanel implements ActionListener {
 		}
 		combo.addActionListener(this);
 		combo.setEnabled(journals!=null);
-		journalManagement.setEnabled(journals != null);
 	}
 
 	public void addJournal(Journal journal){
@@ -125,7 +74,5 @@ public class JournalsGUI extends JPanel implements ActionListener {
 
 		journalGUI.setJournal(journal);
 		journalInputGUI.setJournal(journal);
-
-		details.setEnabled(journal!=null);
 	}
 }
