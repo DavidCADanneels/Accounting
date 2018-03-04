eb4576b76a861d4cb1a6f33e0d59b2be60cc75e0
David C.A. Danneels <daviddafke@msn.com>
2018-03-04 13:49:46 +0100

    add menu Item (no action yet)

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Goods/GoodsMenu.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Goods/GoodsMenu.java
@@ -14,7 +14,7 @@ import static java.util.ResourceBundle.getBundle;
  * Created by ddanneels on 27/12/2015.
  */
 public class GoodsMenu extends JMenu {
-    private JMenuItem articlesTable;
+    private JMenuItem articlesTable, stockTable;
 
     private Articles articles;
     private Contacts contacts;
@@ -23,11 +23,21 @@ public class GoodsMenu extends JMenu {
         super(getBundle("Accounting").getString("GOODS"));
         setMnemonic(KeyEvent.VK_G);
         articlesTable = new JMenuItem(getBundle("Accounting").getString("ARTICLES"));
+        articlesTable.setMnemonic(KeyEvent.VK_A);
         articlesTable.addActionListener(e -> {
             ArticlesGUI.showArticles(articles, contacts).setVisible(true);
         });
         articlesTable.setEnabled(false);
+
+        stockTable = new JMenuItem(getBundle("Accounting").getString("STOCK"));
+        stockTable.setMnemonic(KeyEvent.VK_S);
+        stockTable.addActionListener(e -> {
+//            ArticlesGUI.showArticles(articles, contacts).setVisible(true);
+        });
+        stockTable.setEnabled(false);
+
         add(articlesTable);
+        add(stockTable);
     }
 
     public void setAccounting(Accounting accounting) {
