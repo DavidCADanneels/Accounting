3589d6c45a29fc951f3ec2235e3d76265ab1ca69
David C.A. Danneels <daviddafke@msn.com>
2018-03-11 16:04:23 +0100

    refactor Article: use 2 sales prices (VAT incl)

--- BusinessModel/src/main/java/be/dafke/BusinessModel/Article.java
+++ BusinessModel/src/main/java/be/dafke/BusinessModel/Article.java
@@ -8,7 +8,11 @@ import java.util.function.Predicate;
 public class Article extends BusinessObject{
     private String HSCode = "";
     private Integer vatRate = 6;
+    private Integer itemsPerUnit = 1;
+    private Integer minimumNumberForReduction = 10;
     private BigDecimal purchasePrice = null;
+    private BigDecimal salesPriceSingleWithVat = null;
+    private BigDecimal salesPricePromoWithVat = null;
     private Contact supplier, customer;
 
     public Article(String name){
@@ -33,6 +37,30 @@ public class Article extends BusinessObject{
 
     public void setCustomer(Contact customer) {
         this.customer = customer;
+    }
+
+    public void setItemsPerUnit(Integer itemsPerUnit) {
+        this.itemsPerUnit = itemsPerUnit;
+    }
+
+    public void setMinimumNumberForReduction(Integer minimumNumberForReduction) {
+        this.minimumNumberForReduction = minimumNumberForReduction;
+    }
+
+    public void setSalesPriceSingleWithVat(BigDecimal salesPriceSingleWithVat) {
+        this.salesPriceSingleWithVat = salesPriceSingleWithVat;
+    }
+
+    public void setSalesPricePromoWithVat(BigDecimal salesPricePromoWithVat) {
+        this.salesPricePromoWithVat = salesPricePromoWithVat;
+    }
+
+    public Integer getItemsPerUnit() {
+        return itemsPerUnit;
+    }
+
+    public Integer getMinimumNumberForReduction() {
+        return minimumNumberForReduction;
     }
 
     public Contact getSupplier() {
@@ -101,20 +129,44 @@ public class Article extends BusinessObject{
         return multiply(getPurchaseVat(), number);
     }
 
-    public BigDecimal getSalesPriceWithVat(BigDecimal salesPriceWithoutVat){
-        return salesPriceWithoutVat.multiply(getFactor());
+    public BigDecimal getSalesVat(BigDecimal price){
+        BigDecimal factor = getFactor();
+        BigDecimal withoutVat = price.divide(factor);
+        BigDecimal percentage = getPercentage();
+        BigDecimal vatAmount = withoutVat.multiply(percentage);
+        return vatAmount;
     }
 
-    public BigDecimal getSalesPriceWithoutVat(BigDecimal salesPriceWithVat){
-        return salesPriceWithVat.divide(getFactor());
+    public BigDecimal getSalesPriceSingleWithVat() {
+        return salesPriceSingleWithVat;
     }
 
-    public BigDecimal getSalesPriceWithVat(BigDecimal salesPriceWithoutVat, int number){
-        return multiply(getSalesPriceWithVat(salesPriceWithoutVat), number);
+    public BigDecimal getSalesPricePromoWithVat() {
+        return salesPricePromoWithVat;
     }
 
-    public BigDecimal getSalesPriceWithoutVat(BigDecimal salesPriceWithVat, int number){
-        return multiply(getSalesPriceWithoutVat(salesPriceWithVat), number);
+    public BigDecimal getSalesPriceSingleWithVat(int number){
+        return multiply(salesPriceSingleWithVat, number);
+    }
+
+    public BigDecimal getSalesPricePromoWithVat(int number){
+        return multiply(salesPricePromoWithVat, number);
+    }
+
+    public BigDecimal getSalesPriceSingleWithoutVat(){
+        return salesPriceSingleWithVat.divide(getFactor());
+    }
+
+    public BigDecimal getSalesPricePromoWithoutVat(){
+        return salesPricePromoWithVat.divide(getFactor());
+    }
+
+    public BigDecimal getSalesPriceSingleWithoutVat(int number){
+        return multiply(getSalesPriceSingleWithoutVat(), number);
+    }
+
+    public BigDecimal getSalesPricePromoWithoutVat(int number){
+        return multiply(getSalesPricePromoWithoutVat(), number);
     }
 
     public static Predicate<Article> ofSupplier(Contact supplier) {
