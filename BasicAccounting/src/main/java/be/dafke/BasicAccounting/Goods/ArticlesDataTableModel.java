ad79a079d434394601803db6448dc51729b88424
David C.A. Danneels <daviddafke@msn.com>
2018-12-02 14:58:28 +0100

    get rid of promo/single, use item/unit

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Goods/ArticlesDataTableModel.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Goods/ArticlesDataTableModel.java
@@ -27,13 +27,13 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 	public static int PURCHASE_PRICE_COL = 4;
 	public static int PURCHASE_VAT_COL = 5;
 	public static int SUPPLIER_COL = 6;
-	public static int SALE_SINGLE_EXCL_COL = 7;
-	public static int SALE_PROMO_EXCL_COL = 8;
+	public static int SALE_ITEM_EXCL_COL = 7;
+	public static int SALE_UNIT_EXCL_COL = 8;
 	public static int SALES_VAT_COL = 9;
-	public static int SALE_SINGLE_INCL_COL = 10;
-	public static int SALE_PROMO_INCL_COL = 11;
-	public static int GAIN_ON_SINGLE_COL = 12;
-	public static int GAIN_ON_PROMO_COL = 13;
+	public static int SALE_ITEM_INCL_COL = 10;
+	public static int SALE_UNIT_INCL_COL = 11;
+	public static int GAIN_ON_ITEM_COL = 12;
+	public static int GAIN_ON_UNIT_COL = 13;
 	public static int NR_OF_COL = 14;
 	private final Component parent;
 	private HashMap<Integer,String> columnNames = new HashMap<>();
@@ -57,8 +57,8 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 		editableColumns.add(PURCHASE_VAT_COL);
 		editableColumns.add(SALES_VAT_COL);
 		editableColumns.add(SUPPLIER_COL);
-		editableColumns.add(SALE_SINGLE_INCL_COL);
-		editableColumns.add(SALE_PROMO_INCL_COL);
+		editableColumns.add(SALE_ITEM_INCL_COL);
+		editableColumns.add(SALE_UNIT_INCL_COL);
 	}
 
 	private void setColumnClasses() {
@@ -70,12 +70,12 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 		columnClasses.put(PURCHASE_VAT_COL, Integer.class);
 		columnClasses.put(SALES_VAT_COL, Integer.class);
 		columnClasses.put(SUPPLIER_COL, Contact.class);
-		columnClasses.put(SALE_SINGLE_EXCL_COL, BigDecimal.class);
-		columnClasses.put(SALE_PROMO_EXCL_COL, BigDecimal.class);
-		columnClasses.put(SALE_SINGLE_INCL_COL, BigDecimal.class);
-		columnClasses.put(SALE_PROMO_INCL_COL, BigDecimal.class);
-		columnClasses.put(GAIN_ON_SINGLE_COL, BigDecimal.class);
-		columnClasses.put(GAIN_ON_PROMO_COL, BigDecimal.class);
+		columnClasses.put(SALE_ITEM_EXCL_COL, BigDecimal.class);
+		columnClasses.put(SALE_UNIT_EXCL_COL, BigDecimal.class);
+		columnClasses.put(SALE_ITEM_INCL_COL, BigDecimal.class);
+		columnClasses.put(SALE_UNIT_INCL_COL, BigDecimal.class);
+		columnClasses.put(GAIN_ON_ITEM_COL, BigDecimal.class);
+		columnClasses.put(GAIN_ON_UNIT_COL, BigDecimal.class);
 	}
 
 	private void setColumnNames() {
@@ -87,12 +87,12 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 		columnNames.put(PURCHASE_VAT_COL, getBundle("Accounting").getString("PURCHASE_VAT"));
 		columnNames.put(SALES_VAT_COL, getBundle("Accounting").getString("SALES_VAT"));
 		columnNames.put(SUPPLIER_COL, getBundle("Contacts").getString("SUPPLIER"));
-		columnNames.put(SALE_SINGLE_EXCL_COL, getBundle("Accounting").getString("SALE_SINGLE_EXCL"));
-		columnNames.put(SALE_PROMO_EXCL_COL, getBundle("Accounting").getString("SALE_PROMO_EXCL"));
-		columnNames.put(SALE_SINGLE_INCL_COL, getBundle("Accounting").getString("SALE_SINGLE_INCL"));
-		columnNames.put(SALE_PROMO_INCL_COL, getBundle("Accounting").getString("SALE_PROMO_INCL"));
-		columnNames.put(GAIN_ON_SINGLE_COL, getBundle("Accounting").getString("GAIN_ON_SINGLE"));
-		columnNames.put(GAIN_ON_PROMO_COL, getBundle("Accounting").getString("GAIN_ON_PROMO"));
+		columnNames.put(SALE_ITEM_EXCL_COL, getBundle("Accounting").getString("SALE_ITEM_EXCL"));
+		columnNames.put(SALE_UNIT_EXCL_COL, getBundle("Accounting").getString("SALE_UNIT_EXCL"));
+		columnNames.put(SALE_ITEM_INCL_COL, getBundle("Accounting").getString("SALE_ITEM_INCL"));
+		columnNames.put(SALE_UNIT_INCL_COL, getBundle("Accounting").getString("SALE_UNIT_INCL"));
+		columnNames.put(GAIN_ON_ITEM_COL, getBundle("Accounting").getString("GAIN_ON_ITEM"));
+		columnNames.put(GAIN_ON_UNIT_COL, getBundle("Accounting").getString("GAIN_ON_UNIT"));
 	}
 	// DE GET METHODEN
 // ===============
@@ -123,28 +123,28 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 		if (col == ITEMS_PER_UNIT_COL) {
 			return article.getItemsPerUnit();
 		}
-		if (col == SALE_SINGLE_EXCL_COL) {
+		if (col == SALE_ITEM_EXCL_COL) {
 			BigDecimal salesPriceSingleWithoutVat = article.getSalesPriceItemWithoutVat();
 			return salesPriceSingleWithoutVat!=null?salesPriceSingleWithoutVat:BigDecimal.ZERO;
 		}
-		if (col == SALE_PROMO_EXCL_COL) {
-			BigDecimal salesPricePromoWithoutVat = article.getSalesPriceUnitWithoutVat();
-			return salesPricePromoWithoutVat!=null?salesPricePromoWithoutVat:BigDecimal.ZERO;
+		if (col == SALE_UNIT_EXCL_COL) {
+			BigDecimal salesPriceUnitWithoutVat = article.getSalesPriceUnitWithoutVat();
+			return salesPriceUnitWithoutVat!=null?salesPriceUnitWithoutVat:BigDecimal.ZERO;
 		}
-		if (col == SALE_SINGLE_INCL_COL) {
+		if (col == SALE_ITEM_INCL_COL) {
 			BigDecimal salesPriceSingleWithVat = article.getSalesPriceItemWithVat();
 			return salesPriceSingleWithVat!=null?salesPriceSingleWithVat:BigDecimal.ZERO;
 		}
-		if (col == SALE_PROMO_INCL_COL) {
+		if (col == SALE_UNIT_INCL_COL) {
 			BigDecimal salesPricePromoWithVat = article.getSalesPriceUnitWithVat();
 			return salesPricePromoWithVat!=null?salesPricePromoWithVat:BigDecimal.ZERO;
 		}
-		if (col == GAIN_ON_SINGLE_COL) {
+		if (col == GAIN_ON_ITEM_COL) {
 			BigDecimal salesPriceSingleWithoutVat = article.getSalesPriceItemWithoutVat();
 			if(salesPriceSingleWithoutVat==null) return BigDecimal.ZERO;
 			return article.getItemProfit(salesPriceSingleWithoutVat);
 		}
-		if (col == GAIN_ON_PROMO_COL) {
+		if (col == GAIN_ON_UNIT_COL) {
 			BigDecimal salesPricePromoWithoutVat = article.getSalesPriceUnitWithoutVat();
 			if(salesPricePromoWithoutVat==null) return BigDecimal.ZERO;
 			return article.getUnitProfit(salesPricePromoWithoutVat);
@@ -202,11 +202,11 @@ public class ArticlesDataTableModel extends SelectableTableModel<Article> {
 		if(col == ITEMS_PER_UNIT_COL){
             article.setItemsPerUnit((Integer) value);
 		}
-		if (col == SALE_SINGLE_INCL_COL) {
+		if (col == SALE_ITEM_INCL_COL) {
 			BigDecimal amount = (BigDecimal) value;
 			article.setSalesPriceItemWithVat(amount.setScale(2));
 		}
-		if (col == SALE_PROMO_INCL_COL) {
+		if (col == SALE_UNIT_INCL_COL) {
 			BigDecimal amount = (BigDecimal) value;
 			article.setSalesPriceUnitWithVat(amount.setScale(2));
 		}
