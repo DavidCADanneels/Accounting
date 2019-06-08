a4647428ee9653ebd21fafdbb41ec5b490d61a86
David C.A. Danneels <daviddafke@msn.com>
2019-06-08 18:14:14 +0200

    rename variable

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Trade/AllergenesGUI.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Trade/AllergenesGUI.java
@@ -3,7 +3,6 @@ package be.dafke.BasicAccounting.Trade;
 
 import be.dafke.BasicAccounting.MainApplication.Main;
 import be.dafke.BusinessModel.Allergenes;
-import be.dafke.BusinessModel.Ingredients;
 
 import javax.swing.*;
 import java.util.HashMap;
@@ -11,14 +10,14 @@ import java.util.HashMap;
 import static java.util.ResourceBundle.getBundle;
 
 public class AllergenesGUI extends JFrame {
-    private final AllergenesPanel ingredientsPanel;
+    private final AllergenesPanel allergenesPanel;
 
     private static final HashMap<Allergenes, AllergenesGUI> guis = new HashMap<>();
 
     private AllergenesGUI(Allergenes allergenes) {
         super(getBundle("Accounting").getString("ALLERGENES"));
-        ingredientsPanel = new AllergenesPanel(allergenes);
-        setContentPane(ingredientsPanel);
+        allergenesPanel = new AllergenesPanel(allergenes);
+        setContentPane(allergenesPanel);
         pack();
     }
 
