b8e6ac107eb8c325885b774e8d882a9fd9b8ac1c
David C.A. Danneels <daviddafke@msn.com>
2019-06-15 13:22:45 +0200

    show D/C Totals

--- BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/TransactionOverviewPanel.java
+++ BasicAccounting/src/main/java/be/dafke/BasicAccounting/Journals/TransactionOverviewPanel.java
@@ -9,6 +9,8 @@ import java.awt.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import static java.util.ResourceBundle.getBundle;
+
 public class TransactionOverviewPanel extends JPanel {
     private final SelectableTable<Transaction> transactionOverviewTable;
     private final SelectableTable<Booking> transactionDataTable;
@@ -18,6 +20,7 @@ public class TransactionOverviewPanel extends JPanel {
     private final TransactionOverviewDataModel transactionOverviewDataModel;
     private final TransactionDataModel transactionDataModel;
     private final TransactionOverviewColorRenderer renderer;
+    private final JTextField debet, credit;
 
     public TransactionOverviewPanel() {
 		setLayout(new BorderLayout());
@@ -43,11 +46,26 @@ public class TransactionOverviewPanel extends JPanel {
         transactionOverviewTable.addMouseListener(PopupForTableActivator.getInstance(transactionOverviewPopupMenu, transactionOverviewTable));
         transactionDataTable.addMouseListener(PopupForTableActivator.getInstance(transactionDataPopupMenu, transactionDataTable));
 
-        JScrollPane scrollPane1 = new JScrollPane(transactionOverviewTable);
-        JScrollPane scrollPane2 = new JScrollPane(transactionDataTable);
+        JScrollPane overviewScrollPane = new JScrollPane(transactionOverviewTable);
+        JScrollPane transactionScrollPane = new JScrollPane(transactionDataTable);
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(transactionScrollPane, BorderLayout.CENTER);
+
+        debet = new JTextField(8);
+        credit = new JTextField(8);
+        debet.setEditable(false);
+        credit.setEditable(false);
+        JPanel totalsPanel = new JPanel();
+        totalsPanel.add(new JLabel(getBundle("Accounting").getString("TOTAL_DEBIT")));
+        totalsPanel.add(debet);
+        totalsPanel.add(new JLabel(getBundle("Accounting").getString("TOTAL_CREDIT")));
+        totalsPanel.add(credit);
+
+        bottomPanel.add(totalsPanel, BorderLayout.SOUTH);
+
         JSplitPane center = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-        center.add(scrollPane1, JSplitPane.TOP);
-        center.add(scrollPane2, JSplitPane.BOTTOM);
+        center.add(overviewScrollPane, JSplitPane.TOP);
+        center.add(bottomPanel, JSplitPane.BOTTOM);
 		add(center, BorderLayout.CENTER);
 
         DefaultListSelectionModel selectionModel = new DefaultListSelectionModel();
@@ -59,11 +77,17 @@ public class TransactionOverviewPanel extends JPanel {
 //                } else {
                 Transaction transaction = transactionOverviewTable.getSelectedObject();
                 selectTransaction(transaction);
+                updateTotals(transaction);
 //                }
             }
         });
 
         transactionOverviewTable.setSelectionModel(selectionModel);
+    }
+
+    private void updateTotals(Transaction transaction) {
+        debet.setText(transaction==null?"":transaction.getDebetTotaal().toString());
+        credit.setText(transaction==null?"":transaction.getCreditTotaal().toString());
     }
 
     public void setAccounting(Accounting accounting){
